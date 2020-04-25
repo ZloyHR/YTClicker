24ba5fe7484ff90714593c1c52f2db4a62c1da82
ZloyHR <dobrovolskyivan2002@gmail.com>
2020-04-25 23:31:43 +0300

    Fixed bug with with swipe tabs with achivments

--- Assets/Scripts/AchievmentTabs.cs
+++ Assets/Scripts/AchievmentTabs.cs
@@ -10,10 +10,11 @@ public class AchievmentTabs : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndD
 {
     public GameObject achievmentPrefab;
     public GameObject noAchievment;
+    public GameObject offSecreen;
     private RectTransform _rect;
     private Vector3 _startPosition;
     private Vector2 _delta;
-    private int panelId = 0;
+    private int panelId;
 
     private void Start()
     {
@@ -48,24 +49,29 @@ public class AchievmentTabs : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndD
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        offSecreen.active = true;
         float percent = Mathf.Abs(_delta.x) / _rect.rect.width;
+        Vector3 finishPosition;
         if (percent < 0.2f)
         {
-            _rect.localPosition = _startPosition;
+            finishPosition = _startPosition;
         }
         else
         {
-            if (_delta.x < 0) ++panelId;
-            else --panelId;
-            Vector3 finishPosition = _startPosition + new Vector3( _rect.rect.width, 0, 0) * (_delta.x < 0 ? -1 : 1);
+            int deltaSign = _delta.x < 0 ? 1 : -1;
+            panelId += deltaSign;
             if (panelId < 0 || panelId >= transform.childCount)
             {
-                if (_delta.x < 0) --panelId;
-                else ++panelId;
                 finishPosition = _startPosition;
+                panelId -= deltaSign;
             }
-            StartCoroutine(SmoothSwap(finishPosition, 0.5f));
+            else
+            {
+                finishPosition = _startPosition - new Vector3(_rect.rect.width, 0, 0) * deltaSign;
+            }
+
         }
+        StartCoroutine(SmoothSwap(finishPosition, .5f));
     }
 
     IEnumerator SmoothSwap(Vector3 finishPosition,float second)
@@ -76,5 +82,7 @@ public class AchievmentTabs : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndD
             _rect.localPosition = Vector3.Lerp(startPosition, finishPosition, Mathf.SmoothStep(0,1,i));
             yield return null;
         }
+        _rect.localPosition = finishPosition;
+        offSecreen.active = false;
     }
 }
