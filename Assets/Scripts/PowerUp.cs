3a62eaaed24ee457d8ae6fb97277867be06b7865
ZloyHR <dobrovolskyivan2002@gmail.com>
2020-04-25 21:42:23 +0300

    Rollbacked to stable script

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -1,4 +1,4 @@
-﻿﻿﻿using System;
+﻿﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,28 +9,37 @@ public class PowerUp : MonoBehaviour
     public GameObject bar;
     public float reloadTime;
     public float workTime;
-
     private void Awake()
     {
         StartCoroutine(PowerUpReload(reloadTime));
     }
 
-    public void PowerUpUse()
+    public void StartPowerUp()
     {
-        StartCoroutine(PowerUpReload(reloadTime,workTime));
+        StartCoroutine(PowerUpUse(workTime));
+    }
+    
+    public IEnumerator PowerUpUse(float seconds)
+    {
+        GetComponent<Button>().interactable = false;
+        Currency pviewsPerClick = ClickHandler.viewsPerClick;
+        ClickHandler.viewsPerClick += pviewsPerClick;
+        yield return new WaitForSeconds(seconds);
+        ClickHandler.viewsPerClick -= pviewsPerClick;
+        StartCoroutine(PowerUpReload(reloadTime));
     }
     
     public IEnumerator PowerUpReload(float seconds,float wait = 0)
     {
+        GetComponent<Button>().interactable = false;
         if(wait > 0)yield return new WaitForSeconds(wait);
-        gameObject.active = false;
         for (float i = 0; i < 1; i += Time.deltaTime / seconds)
         {
             bar.GetComponent<Image>().fillAmount = i;
             yield return null;
         }
         bar.GetComponent<Image>().fillAmount = 1;
-        gameObject.active = true;
+        GetComponent<Button>().interactable = true;
         
     }
 }
